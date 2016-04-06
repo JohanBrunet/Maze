17c855ef02c7cac041e12692b741b4a7bf41938b
JohanBrunet <brunet.johan.23@gmail.com>
2016-04-06 16:00:14 +0200

    Create constructor

--- Maze/src/maze/Maze.java
+++ Maze/src/maze/Maze.java
@@ -1,5 +1,27 @@
 package maze;
 
 public class Maze {
+	private int height;
+	private int width;
+	
+	public Maze(int height, int widht) {
+		this.height = height;
+		this.width = widht;
+	}
 
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
 }
