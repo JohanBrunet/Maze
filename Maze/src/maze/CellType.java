77315a4e8e51176d4b82288f0ede694d178c8896
JohanBrunet <brunet.johan.23@gmail.com>
2016-04-06 16:40:35 +0200

    Add enum CellType
    CellType represents the type of the cell

---
+++ Maze/src/maze/CellType.java
@@ -0,0 +1,8 @@
+package maze;
+
+public enum CellType {
+	A,
+	D,
+	E,
+	W;
+}
--- Maze/src/maze/Maze.java
+++ Maze/src/maze/Maze.java
@@ -1,12 +1,22 @@
 package maze;
 
-public class Maze {
+import graph.GraphImp;
+
+public class Maze extends GraphImp {
 	private int height;
 	private int width;
+	private Cell[][] cells;
+	private Cell departure;
+	private Cell arrival;
 	
-	public Maze(int height, int widht) {
+	public Maze(int height, int width, Cell departure, Cell arrival) {
+		super(height*width, height*width*4, false);
 		this.height = height;
-		this.width = widht;
+		this.width = width;
+		this.departure = departure;
+		this.arrival = arrival;
+		this.cells = new Cell[height][width];
+		initMaze(departure, arrival);
 	}
 
 	public int getWidth() {
@@ -23,5 +33,28 @@ public class Maze {
 
 	public void setHeight(int height) {
 		this.height = height;
+	}
+	
+	private void initMaze(Cell departure, Cell arrival) {
+		int i;
+		int j;
+		for (i = 0; i < cells.length; i++) {
+			for (j = 0; j < cells.length; j++) {
+				if (departure.getX() == i && departure.getY() == j) {
+					this.cells[i][j] = new Cell(i, j, CellType.D);
+				}
+				else if (arrival.getX() == i && arrival.getY() == j) {
+					this.cells[i][j] = new Cell(i, j, CellType.A);
+				}
+				else {
+					this.cells[i][j] = new Cell(i, j, CellType.W);
+				}
+			}
+		}
+	}
+	
+	public String toString() {
+		//TODO implement method
+		return null;
 	}
 }
