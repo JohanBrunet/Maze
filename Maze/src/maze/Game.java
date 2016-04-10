518209b6a90ae268a68b93d20812878d806693ce
JohanBrunet <brunet.johan.23@gmail.com>
2016-04-10 15:40:40 +0200

    Update main
    Create method that handles the creation of the walkable cells

--- Maze/src/maze/Game.java
+++ Maze/src/maze/Game.java
@@ -1,5 +1,6 @@
 package maze;
 
+import graph.Edge;
 import graph.Vertex;
 
 public class Game {
@@ -9,6 +10,14 @@ public class Game {
 		Cell arrival = new Cell(2, 2, CellType.A, false);
 		Maze maze = new Maze(3, 3);
 		maze.initMaze(departure, arrival);
+		maze.setWalkableCells(createWalkableCells());
+		maze.toString();
+		System.out.println("Departure : " + departure.displayCoord());
+		System.out.println("Arrival : " + arrival.displayCoord());
+		Vertex[] neighbors = maze.getNeighboors(arrival);
+	}
+	
+	public static Cell[] createWalkableCells() {
 		Cell[] walkableCells = new Cell[6];
 		walkableCells[0] = new Cell(0, 1, CellType.E, false);
 		walkableCells[1] = new Cell(0, 2, CellType.E, false);
@@ -16,12 +25,11 @@ public class Game {
 		walkableCells[3] = new Cell(1, 2, CellType.E, false);
 		walkableCells[4] = new Cell(2, 0, CellType.E, false);
 		walkableCells[5] = new Cell(2, 1, CellType.E, false);
-		maze.setWalkableCells(walkableCells);
-		maze.toString();
-		System.out.println("Departure : " + departure.displayCoord());
-		System.out.println("Arrival : " + arrival.displayCoord());
-		//Vertex[] neighbors = maze.getNeighboors(arrival);
-		//for (Vertex v : neighbors) {	
-		//}
+		return walkableCells;
+	}
+	
+	public static Edge[] setEdges() {
+		//TODO implement method to set the edges between walkable cells
+		return null;
 	}
 }
