d06ccfc407742a179b61d9b1afec83f4500bb61e
JohanBrunet <brunet.johan.23@gmail.com>
2016-04-10 20:40:03 +0200

    Implment method setEdges() in main class

--- Maze/src/maze/Game.java
+++ Maze/src/maze/Game.java
@@ -1,24 +1,27 @@
 package maze;
 
 import graph.Edge;
+import graph.UndirectedEdge;
 import graph.Vertex;
 
 public class Game {
 	private static Cell[] walkableCells;
+	private static Maze maze;
 	
 	public static void main(String[] args) {
 		Cell departure = new Cell(0, 0, CellType.D, false);
 		Cell arrival = new Cell(2, 2, CellType.A, false);
-		Maze maze = new Maze(3, 3);
+		maze = new Maze(3, 3);
 		maze.initMaze(departure, arrival);
 		maze.setWalkableCells(createWalkableCells());
+		setEdges();
 		maze.toString();
 		System.out.println("Departure : " + departure.displayCoord());
 		System.out.println("Arrival : " + arrival.displayCoord());
 		Vertex[] neighbors = maze.getNeighboors(arrival);
 	}
 	
-	public static Cell[] createWalkableCells() {
+	private static Cell[] createWalkableCells() {
 		walkableCells = new Cell[6];
 		walkableCells[0] = new Cell(0, 1, CellType.E, false);
 		walkableCells[1] = new Cell(0, 2, CellType.E, false);
@@ -29,12 +32,18 @@ public class Game {
 		return walkableCells;
 	}
 	
-	public static Edge[] setEdges() {
+	private static void setEdges() {
 		//TODO implement method to set the edges between walkable cells
-		int i;
+		int i, j;
 		for (i = 0; i < walkableCells.length; i++) {
-			
+			for (j = 0; j < walkableCells.length; j++) {
+				if ((walkableCells[i].getX() == walkableCells[j].getX()) && (walkableCells[i].getY() == walkableCells[j].getY()-1)) {
+					maze.addEdge(new UndirectedEdge(walkableCells[i], walkableCells[j]));
+				}
+				else if ((walkableCells[i].getX() == walkableCells[j].getX()-1) && (walkableCells[i].getY() == walkableCells[j].getY())) {
+					maze.addEdge(new UndirectedEdge(walkableCells[i], walkableCells[j]));
+				}
+			}
 		}
-		return null;
 	}
 }
